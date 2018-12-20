a85854eb85f4afe699336fb853575aaca8ae10f8
Andrew Zah <zah@andrewzah.com>
2018-12-20 13:39:42 -0500

    Add pure korean parsing. ->
    * Refactor code to take sino/pure differences
      into account.
    * Make `hangul_to_int/2` take a generic {integer}
    * Tests:
      * Add pure korean tests for nums 1-99
      * Update testcases to use nums instead of strings
      * Change test numbers to use underlines instead of
      commas

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,67 @@
+extern crate num;
+
 mod numbers;
 mod place;
 mod block;
 
 use std::cmp::{min};
+use std::cmp::Ord;
+use num::{FromPrimitive, PrimInt};
 
-pub fn str_to_hangul(input: &str) -> String {
-    let nums: Vec<char> = input
+pub fn hangul_from_string(input: String, is_sino: bool) -> String {
+    hangul_from_int(input.parse::<u64>().unwrap(), is_sino)
+}
+
+pub fn hangul_from_int<T>(input: T, is_sino: bool) -> String
+    where T: PrimInt + ToString + FromPrimitive
+{
+    validate_sino(input, is_sino);
+    let prepared_input = prepare_input(input);
+    match is_sino {
+        true => parse_hangul_sino(prepared_input),
+        false => parse_hangul_pure(prepared_input),
+    }
+}
+
+fn validate_sino<T>(input: T, is_sino: bool)
+    where T: PrimInt + ToString + FromPrimitive
+{
+    if is_sino == false && input > FromPrimitive::from_u64(99).unwrap() {
+        panic!("Pure korean numbers only go up to 99.");
+    }
+    if input < FromPrimitive::from_u64(0).unwrap() {
+        panic!("Input cannot be negative.")
+    }
+}
+
+fn prepare_input<T>(input: T) -> Vec<char>
+    where T: PrimInt + ToString + Ord
+{
+    let nums = input
+        .to_string()
         .replace(",", "")
         .chars()
         .rev()
         .collect();
 
-    calculate(nums)
+    nums
 }
 
-fn calculate(numbers: Vec<char>) -> String {
+fn parse_hangul_sino(numbers: Vec<char>) -> String {
     let len = numbers.len() - 1;
-    let mut output = String::from("");
+    let mut output = String::new();
     let mut iter = numbers.iter().enumerate();
 
     while let Some((idx, input_num)) = iter.next() {
         let remaining = len - idx;
         let min_peek_len = min(remaining, 3);
-        let mut num = numbers::SinoNumber::from_char(input_num).unwrap();
+        let mut num = numbers::KoreanNumberSino::from_char(input_num).unwrap();
 
         if min_peek_len != 0 && num == 0 {
             let mut zeroes = 1;
             while let Some((_, next_num)) = iter.next() {
                 if *next_num != '0' {
-                    num = numbers::SinoNumber::from_char(next_num).unwrap();
+                    num = numbers::KoreanNumberSino::from_char(next_num).unwrap();
                     break;
                 }
                 zeroes += 1;
@@ -81,4 +114,43 @@ fn calculate(numbers: Vec<char>) -> String {
     }
 
     output.chars().rev().collect::<String>()
+}
+
+fn parse_hangul_pure(numbers: Vec<char>) -> String {
+    let mut output = String::new();
+    let mut iter = numbers.iter().enumerate().peekable();
+
+    println!("\nnumbers: {:?}", numbers);
+    while let Some((idx, input_num)) = iter.next() {
+        match (idx, input_num) {
+            (0, '0') => {
+                let (_, next_num) = iter.next().unwrap();
+                let new_input = format!("{}{}", next_num, "0");
+                let num = numbers::KoreanNumberPure::from_str(&new_input).unwrap();
+
+                output.push_str(num.to_str());
+                return output;
+            },
+            (0, _) => {
+                if let Some((_, next_num)) = iter.peek() {
+                    let next_input = format!("{}{}", next_num, "0");
+                    let next_num = numbers::KoreanNumberPure::from_str(&next_input).unwrap();
+                    output.push_str(next_num.to_str());
+
+                    let input_num = numbers::KoreanNumberPure::from_char(input_num).unwrap();
+                    output.push_str(input_num.to_str());
+                    return output;
+                } else {
+                    let num = numbers::KoreanNumberPure::from_char(input_num).unwrap();
+                    output.push_str(num.to_str());
+                }
+            },
+            (1, _) => {
+                let num = numbers::KoreanNumberPure::from_char(input_num).unwrap();
+                output.push_str(num.to_str());
+            }
+            (_, _) => {}
+        }
+    }
+    output
 }
--- src/numbers.rs
+++ src/numbers.rs
@@ -1,5 +1,4 @@
-#[allow(dead_code)]
-pub enum SinoNumber {
+pub enum KoreanNumberSino {
     Zero,
     One,
     Two,
@@ -10,83 +9,84 @@ pub enum SinoNumber {
     Seven,
     Eight,
     Nine,
-}
-
-impl PartialEq<i32> for SinoNumber {
-    fn eq(&self, other: &i32) -> bool {
-        self.to_i32().eq(other)
-    }
-}
-
-impl SinoNumber {
-    pub fn from_char(c: &char) -> Option<SinoNumber> {
-        match c {
-            '0' => Some(SinoNumber::Zero),
-            '1' => Some(SinoNumber::One),
-            '2' => Some(SinoNumber::Two),
-            '3' => Some(SinoNumber::Three),
-            '4' => Some(SinoNumber::Four),
-            '5' => Some(SinoNumber::Five),
-            '6' => Some(SinoNumber::Six),
-            '7' => Some(SinoNumber::Seven),
-            '8' => Some(SinoNumber::Eight),
-            '9' => Some(SinoNumber::Nine),
-            _ => None
-        }
-    }
-
     #[allow(dead_code)]
-    pub fn to_i32(&self) -> i32 {
-        match self {
-            &SinoNumber::Zero => 0,
-            &SinoNumber::One => 1,
-            &SinoNumber::Two => 2,
-            &SinoNumber::Three => 3,
-            &SinoNumber::Four => 4,
-            &SinoNumber::Five => 5,
-            &SinoNumber::Six => 6,
-            &SinoNumber::Seven => 7,
-            &SinoNumber::Eight => 8,
-            &SinoNumber::Nine => 9,
+    Ten,
+}
+
+impl PartialEq<i8> for KoreanNumberSino {
+    fn eq(&self, other: &i8) -> bool {
+        self.to_i8().eq(other)
+    }
+}
+
+impl KoreanNumberSino {
+    pub fn from_char(c: &char) -> Option<KoreanNumberSino> {
+        match c {
+            '0' => Some(KoreanNumberSino::Zero),
+            '1' => Some(KoreanNumberSino::One),
+            '2' => Some(KoreanNumberSino::Two),
+            '3' => Some(KoreanNumberSino::Three),
+            '4' => Some(KoreanNumberSino::Four),
+            '5' => Some(KoreanNumberSino::Five),
+            '6' => Some(KoreanNumberSino::Six),
+            '7' => Some(KoreanNumberSino::Seven),
+            '8' => Some(KoreanNumberSino::Eight),
+            '9' => Some(KoreanNumberSino::Nine),
+            _ => None
         }
     }
 
     pub fn to_str(&self) -> &str {
         match self {
-            &SinoNumber::Zero => "공",
-            &SinoNumber::One => "일",
-            &SinoNumber::Two => "이",
-            &SinoNumber::Three => "삼",
-            &SinoNumber::Four => "사",
-            &SinoNumber::Five => "오",
-            &SinoNumber::Six => "육",
-            &SinoNumber::Seven => "칠",
-            &SinoNumber::Eight => "팔",
-            &SinoNumber::Nine => "구",
+            &KoreanNumberSino::Zero => "",
+            &KoreanNumberSino::One => "일",
+            &KoreanNumberSino::Two => "이",
+            &KoreanNumberSino::Three => "삼",
+            &KoreanNumberSino::Four => "사",
+            &KoreanNumberSino::Five => "오",
+            &KoreanNumberSino::Six => "육",
+            &KoreanNumberSino::Seven => "칠",
+            &KoreanNumberSino::Eight => "팔",
+            &KoreanNumberSino::Nine => "구",
+            &KoreanNumberSino::Ten => "십",
+        }
+    }
+
+    pub fn to_i8(&self) -> i8 {
+        match self {
+            &KoreanNumberSino::Zero => 0,
+            &KoreanNumberSino::One => 1,
+            &KoreanNumberSino::Two => 2,
+            &KoreanNumberSino::Three => 3,
+            &KoreanNumberSino::Four => 4,
+            &KoreanNumberSino::Five => 5,
+            &KoreanNumberSino::Six => 6,
+            &KoreanNumberSino::Seven => 7,
+            &KoreanNumberSino::Eight => 8,
+            &KoreanNumberSino::Nine => 9,
+            &KoreanNumberSino::Ten => 10,
         }
     }
 
     #[allow(dead_code)]
     pub fn to_hanja(&self) -> &str {
         match self {
-            &SinoNumber::Zero => "零/〇",
-            &SinoNumber::One => "一",
-            &SinoNumber::Two => "二",
-            &SinoNumber::Three => "三",
-            &SinoNumber::Four => "四",
-            &SinoNumber::Five => "五",
-            &SinoNumber::Six => "六",
-            &SinoNumber::Seven => "七",
-            &SinoNumber::Eight => "八",
-            &SinoNumber::Nine => "九",
-            //&SinoNumber::Ten => "十",
+            &KoreanNumberSino::Zero => "零/〇",
+            &KoreanNumberSino::One => "一",
+            &KoreanNumberSino::Two => "二",
+            &KoreanNumberSino::Three => "三",
+            &KoreanNumberSino::Four => "四",
+            &KoreanNumberSino::Five => "五",
+            &KoreanNumberSino::Six => "六",
+            &KoreanNumberSino::Seven => "七",
+            &KoreanNumberSino::Eight => "八",
+            &KoreanNumberSino::Nine => "九",
+            &KoreanNumberSino::Ten => "十",
         }
     }
-
 }
 
-#[allow(dead_code)]
-pub enum KoreanNumber {
+pub enum KoreanNumberPure {
     Zero,
     One,
     Two,
@@ -108,80 +108,59 @@ pub enum KoreanNumber {
     Ninety,
 }
 
-impl KoreanNumber {
-    #[allow(dead_code)]
-    pub fn from_str(s: &str) -> Option<KoreanNumber> {
-        match s {
-            "0" => Some(KoreanNumber::Zero),
-            "1" => Some(KoreanNumber::One),
-            "2" => Some(KoreanNumber::Two),
-            "3" => Some(KoreanNumber::Three),
-            "4" => Some(KoreanNumber::Four),
-            "5" => Some(KoreanNumber::Five),
-            "6" => Some(KoreanNumber::Six),
-            "7" => Some(KoreanNumber::Seven),
-            "8" => Some(KoreanNumber::Eight),
-            "9" => Some(KoreanNumber::Nine),
-            "10" => Some(KoreanNumber::Ten),
-            "20" => Some(KoreanNumber::Twenty),
-            "30" => Some(KoreanNumber::Thirty),
-            "40" => Some(KoreanNumber::Forty),
-            "50" => Some(KoreanNumber::Fifty),
-            "60" => Some(KoreanNumber::Sixty),
-            "70" => Some(KoreanNumber::Seventy),
-            "80" => Some(KoreanNumber::Eighty),
-            "90" => Some(KoreanNumber::Ninety),
+impl KoreanNumberPure {
+    pub fn from_char(c: &char) -> Option<KoreanNumberPure> {
+        match c {
+            '0' => Some(KoreanNumberPure::Zero),
+            '1' => Some(KoreanNumberPure::One),
+            '2' => Some(KoreanNumberPure::Two),
+            '3' => Some(KoreanNumberPure::Three),
+            '4' => Some(KoreanNumberPure::Four),
+            '5' => Some(KoreanNumberPure::Five),
+            '6' => Some(KoreanNumberPure::Six),
+            '7' => Some(KoreanNumberPure::Seven),
+            '8' => Some(KoreanNumberPure::Eight),
+            '9' => Some(KoreanNumberPure::Nine),
             _ => None
         }
     }
 
-    #[allow(dead_code)]
-    pub fn to_str(&self) -> &str {
-        match self {
-            &KoreanNumber::Zero => "영/제로",
-            &KoreanNumber::One => "하나",
-            &KoreanNumber::Two => "둘",
-            &KoreanNumber::Three => "셋",
-            &KoreanNumber::Four => "넷",
-            &KoreanNumber::Five => "다섯",
-            &KoreanNumber::Six => "여섯",
-            &KoreanNumber::Seven => "일곱",
-            &KoreanNumber::Eight => "여덟",
-            &KoreanNumber::Nine => "아홉",
-            &KoreanNumber::Ten => "열",
-            &KoreanNumber::Twenty => "스무",
-            &KoreanNumber::Thirty => "서른",
-            &KoreanNumber::Forty => "마흔",
-            &KoreanNumber::Fifty => "쉰",
-            &KoreanNumber::Sixty => "예순",
-            &KoreanNumber::Seventy => "일흔",
-            &KoreanNumber::Eighty => "여든",
-            &KoreanNumber::Ninety => "아흔",
+    pub fn from_str(s: &str) -> Option<KoreanNumberPure> {
+        match s {
+            "10" => Some(KoreanNumberPure::Ten),
+            "20" => Some(KoreanNumberPure::Twenty),
+            "30" => Some(KoreanNumberPure::Thirty),
+            "40" => Some(KoreanNumberPure::Forty),
+            "50" => Some(KoreanNumberPure::Fifty),
+            "60" => Some(KoreanNumberPure::Sixty),
+            "70" => Some(KoreanNumberPure::Seventy),
+            "80" => Some(KoreanNumberPure::Eighty),
+            "90" => Some(KoreanNumberPure::Ninety),
+            _ => None
         }
     }
 
-    #[allow(dead_code)]
-    pub fn to_i32(&self) -> i32 {
+    pub fn to_str(&self) -> &str {
         match self {
-            &KoreanNumber::Zero => 0,
-            &KoreanNumber::One => 1,
-            &KoreanNumber::Two => 2,
-            &KoreanNumber::Three => 3,
-            &KoreanNumber::Four => 4,
-            &KoreanNumber::Five => 5,
-            &KoreanNumber::Six => 6,
-            &KoreanNumber::Seven => 7,
-            &KoreanNumber::Eight => 8,
-            &KoreanNumber::Nine => 9,
-            &KoreanNumber::Ten => 20,
-            &KoreanNumber::Twenty => 20,
-            &KoreanNumber::Thirty => 30,
-            &KoreanNumber::Forty => 40,
-            &KoreanNumber::Fifty => 50,
-            &KoreanNumber::Sixty => 60,
-            &KoreanNumber::Seventy => 70,
-            &KoreanNumber::Eighty => 80,
-            &KoreanNumber::Ninety => 80,
+            &KoreanNumberPure::Zero => "영/제로",
+            &KoreanNumberPure::One => "하나",
+            &KoreanNumberPure::Two => "둘",
+            &KoreanNumberPure::Three => "셋",
+            &KoreanNumberPure::Four => "넷",
+            &KoreanNumberPure::Five => "다섯",
+            &KoreanNumberPure::Six => "여섯",
+            &KoreanNumberPure::Seven => "일곱",
+            &KoreanNumberPure::Eight => "여덟",
+            &KoreanNumberPure::Nine => "아홉",
+            &KoreanNumberPure::Ten => "열",
+            &KoreanNumberPure::Twenty => "스물",
+            &KoreanNumberPure::Thirty => "서른",
+            &KoreanNumberPure::Forty => "마흔",
+            &KoreanNumberPure::Fifty => "쉰",
+            &KoreanNumberPure::Sixty => "예순",
+            &KoreanNumberPure::Seventy => "일흔",
+            &KoreanNumberPure::Eighty => "여든",
+            &KoreanNumberPure::Ninety => "아흔",
         }
     }
 }
