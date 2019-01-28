1465bd021f3275305f1b66ca1ccb7f8b72cab44a
Andrew Zah <zah@andrewzah.com>
2019-01-28 15:28:52 +0900

    fix x00xx nums not spacing right

--- src/parse.rs
+++ src/parse.rs
@@ -17,15 +17,17 @@ pub fn parse_hangeul_sino(numbers: Vec<char>) -> String {
             continue;
         }
 
-        let remaining = len - idx;
+        let mut mut_idx = idx;
+        let remaining = len - mut_idx;
         let min_peek_len = min(remaining, 3);
         let mut num = numbers::KoreanNumberSino::from_char(input_num).unwrap();
 
         if min_peek_len != 0 && num == 0 {
             let mut zeroes = 1;
-            while let Some((_, next_num)) = iter.next() {
+            while let Some((next_idx, next_num)) = iter.next() {
                 if *next_num != '0' {
                     num = numbers::KoreanNumberSino::from_char(next_num).unwrap();
+                    mut_idx = next_idx;
                     break;
                 }
                 zeroes += 1;
@@ -34,20 +36,48 @@ pub fn parse_hangeul_sino(numbers: Vec<char>) -> String {
 
             if let Some(block) = block::Block::from_usize(zeroes) {
                 // special edge case for 만
-                if idx == len-1 && output.chars().collect::<Vec<char>>()[0] != ' ' && block.to_str() == "만" {
-                    output.push(' ');
+                if block.to_str() == "만" {
+                    if output != "" {
+                        if mut_idx == len - 1 && output.chars().collect::<Vec<char>>()[0] != ' '  {
+                            if let Some((_, peek_num)) = iter.peek() {
+                                match *peek_num {
+                                    '0'..='9' => { output.push(' '); },
+                                    _ => {}
+                                }
+                            }
+                        }
+                    }
+
+                    if num != 1 && mut_idx != len - 1 && output != "" {
+                        output.push(' ');
+                    } 
                 }
                 output.push_str(block.to_str());
 
-                if num != 1 || block.to_str() != "만" {
-                    output.push_str(num.to_str());
+                if let Some((_, peek_num)) = iter.peek() {
+                    match *peek_num {
+                        '0'..='9' => { output.push_str(num.to_str()); },
+                        _ => {}
+                    }
+                } else {
+                    if num != 1 || block.to_str() != "만" {
+                        output.push_str(num.to_str());
+                    }
                 }
+
                 continue;
             } else {
                 let zmod = zeroes % 4;
                 if zeroes >= 4 {
                     let block = block::Block::from_usize(zeroes - zmod).unwrap();
-                    output.push_str(block.to_str());
+                    if output.len() > 2 {
+                        let chars = output.chars().rev().collect::<String>();
+                        if chars[3..6] != *"만" {
+                            output.push_str(block.to_str());
+                        }
+                    } else {
+                        output.push_str(block.to_str());
+                    }
                 }
 
                 let place = place::Place::from_usize(zeroes % 4).unwrap();
@@ -61,7 +91,7 @@ pub fn parse_hangeul_sino(numbers: Vec<char>) -> String {
             }
         }
 
-        let modulo = idx % 4;
+        let modulo = mut_idx % 4;
         match modulo {
             1|2|3 => {
                 let place = place::Place::from_usize(modulo).unwrap();
@@ -72,8 +102,8 @@ pub fn parse_hangeul_sino(numbers: Vec<char>) -> String {
                 }
             },
             _ => {
-                if idx != 0 {
-                    let block = block::Block::from_usize(idx)
+                if mut_idx != 0 {
+                    let block = block::Block::from_usize(mut_idx)
                         .expect("Block counter doesn't go high enough for this...");
                     output.push_str(&block.to_str_with_space());
                     if num != 1 || block.to_str() != "만" || remaining > 0 {
